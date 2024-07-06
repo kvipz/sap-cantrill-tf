74003205110685df673c441d2aba6f5107f1ff7d
Vipin K <k.vipz88@gmail.com>
2024-07-07 01:49:03 +0530

    Cloudfront: Adds custom domain name to distribution

--- cloudfront/data.tf
+++ cloudfront/data.tf
@@ -2,8 +2,14 @@ locals {
   website_endpoint   = aws_cloudformation_stack.one_click_deployment.outputs["WebsiteEndPoint"]
   bucket_name        = split(".", split("/", local.website_endpoint)[2])[0]
   bucket_domain_name = "${local.bucket_name}.s3.us-east-1.amazonaws.com"
+  custom_domain_name = "merlin.feets2cloud.com"  # Change this with your own domain name
 }
 
 data "aws_cloudfront_cache_policy" "managed_caching_optimized" {
   name = "Managed-CachingOptimized"
+}
+
+data "aws_route53_zone" "feets2cloud" {
+  name         = "feets2cloud.com"
+  private_zone = false
 }
--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -8,6 +8,7 @@ resource "aws_cloudfront_distribution" "s3_origin_top10cats" {
 
   enabled             = true
   default_root_object = "index.html"
+  aliases             = [local.custom_domain_name]
 
   origin {
     domain_name = local.bucket_domain_name
@@ -25,7 +26,10 @@ resource "aws_cloudfront_distribution" "s3_origin_top10cats" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = aws_acm_certificate.merlin.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 
   restrictions {
@@ -33,5 +37,43 @@ resource "aws_cloudfront_distribution" "s3_origin_top10cats" {
       restriction_type = "none"
       locations        = []
     }
+  }
+}
+
+resource "aws_acm_certificate" "merlin" {
+  domain_name       = local.custom_domain_name
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "merlin_acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.merlin.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.feets2cloud.zone_id
+}
+
+resource "aws_acm_certificate_validation" "merlin" {
+  certificate_arn         = aws_acm_certificate.merlin.arn
+  validation_record_fqdns = [for record in aws_route53_record.merlin_acm_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "merlin" {
+  zone_id = data.aws_route53_zone.feets2cloud.id
+  name    = "merlin.feets2cloud.com"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.s3_origin_top10cats.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_origin_top10cats.hosted_zone_id
+    evaluate_target_health = true
   }
 }
